a6a4430c16cfdc85eb365a142f8675b1b0b01068
AlexKennethMiles <apuchkov17@gmail.com>
2023-05-20 23:12:06 +0300

    380. Insert Delete GetRandom O(1) (remove old comments)

--- src/test/java/tasks/leetcode/array/RandomizedSetTest.java
+++ src/test/java/tasks/leetcode/array/RandomizedSetTest.java
@@ -22,12 +22,12 @@ class RandomizedSetTest {
     @Test
     public void leetCodeExample10() {
         RandomizedSet randomizedSet = new RandomizedSet();
-        assertThat(randomizedSet.insert(0)).isTrue(); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
-        assertThat(randomizedSet.insert(1)).isTrue(); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
-        assertThat(randomizedSet.remove(0)).isTrue(); // Returns false as 2 does not exist in the set.
-        assertThat(randomizedSet.insert(2)).isTrue(); // Inserts 2 to the set, returns true. Set now contains [1,2].
-        assertThat(randomizedSet.remove(1)).isTrue(); // Removes 1 from the set, returns true. Set now contains [2].
+        assertThat(randomizedSet.insert(0)).isTrue();
+        assertThat(randomizedSet.insert(1)).isTrue();
+        assertThat(randomizedSet.remove(0)).isTrue();
+        assertThat(randomizedSet.insert(2)).isTrue();
+        assertThat(randomizedSet.remove(1)).isTrue();
         int rand = randomizedSet.getRandom();
-        assertThat(rand).isEqualTo(2); // getRandom() should return either 1 or 2 randomly.
+        assertThat(rand).isEqualTo(2);
     }
 }
