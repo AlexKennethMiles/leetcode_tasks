daf7df3675c0c1550ce1d8fdf4c5b94f12484ecf
AlexKennethMiles <apuchkov17@gmail.com>
2023-07-17 21:02:54 +0300

    219. Contains Duplicate II (optimisation)

--- src/main/java/tasks/leetcode/hashmap/ContainsDuplicateII.java
+++ src/main/java/tasks/leetcode/hashmap/ContainsDuplicateII.java
@@ -1,21 +1,29 @@
 package tasks.leetcode.hashmap;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * === Мапа и расстояние между значениями по индексу ===
+ * +++ Оптимизация HashSet +++
+ * +=+ Либо HashMap заданной размерности nums.length
  */
 public class ContainsDuplicateII {
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        int slideWindow = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+            // Если не можем добавить, значит нашли дубликат в пределах k
+            if (!set.add(nums[i])) {
+                return true;
             }
-            map.put(nums[i], i);
+            // Если Set стал больше допустимого расстояния
+            if (set.size() > k) {
+                // Сдвигаем окно на один вправо, удаляя из Set с начала элементы
+                set.remove(nums[slideWindow++]);
+            }
         }
         return false;
     }
