8f6a91fa1d3fb29fc79152510041df17961138cb
AlexKennethMiles <apuchkov17@gmail.com>
2023-08-13 23:45:39 +0300

    112. Path Sum

---
+++ src/main/java/tasks/leetcode/binarytree/general/PathSum.java
@@ -0,0 +1,15 @@
+package tasks.leetcode.binarytree.general;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        } else {
+            return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+        }
+    }
+}
